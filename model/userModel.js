fbb25826c170887bef73afa8510ddd8d0f9b330c
djjniubi <2861606659@qq.com>
2023-02-07 17:29:34 +0800

    ”

---
+++ model/userModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+//后台管理及用户表模型
+const UserType={
+    username:String,
+    password:String,
+    identity:Number
+}
+const schema = mongoose.Schema
+const UserModel=mongoose.model("user",new schema(UserType))
+
+export default UserModel
